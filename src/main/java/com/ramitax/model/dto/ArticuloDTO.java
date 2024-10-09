2aeb924502f7e467216896bb30c3bb11b3b6e803
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-09 10:45:32 -0300

    Añade campos al DTO de Articulo

--- src/main/java/com/ramitax/model/dto/ArticuloDTO.java
+++ src/main/java/com/ramitax/model/dto/ArticuloDTO.java
@@ -1,2 +1,17 @@
-package com.ramitax.model.dto;public class ArticuloDTO {
+package com.ramitax.model.dto;
+
+import com.ramitax.enumerated.Sector;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ArticuloDTO {
+
+    private String nombre;
+    private String marca;
+    private Double contenido;
+    private Double iva;
+    private Sector sector;
+
 }

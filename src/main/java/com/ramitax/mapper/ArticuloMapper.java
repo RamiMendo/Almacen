111017d0c4d105c99f92aab77f0d41d6854b5af3
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-09 14:56:31 -0300

    Se agrega metodo mappeador a la interfaz ArticuloMapper

--- src/main/java/com/ramitax/mapper/ArticuloMapper.java
+++ src/main/java/com/ramitax/mapper/ArticuloMapper.java
@@ -1,7 +1,14 @@
 package com.ramitax.mapper;
 
+import com.ramitax.model.Articulo;
+import com.ramitax.model.dto.ArticuloDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ArticuloMapper {
+
+    ArticuloMapper INSTANCE = Mappers.getMapper(ArticuloMapper.class);
+
+    Articulo articuloDtoToArticulo(ArticuloDTO dto);
 }

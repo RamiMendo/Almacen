eb5ba63008d7b8c0b9dc54cd4b5a3f73f453ba9d
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-09 11:27:56 -0300

    Creacion Patron DTO de Cliente

---
+++ src/main/java/com/ramitax/model/dto/ClienteDTO.java
@@ -0,0 +1,11 @@
+package com.ramitax.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ClienteDTO extends EntidadDTO {
+
+    private String dni;
+}

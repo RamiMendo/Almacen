c31225122482a2f4212f9adddcab2b5b0a04881f
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-11 11:44:54 -0300

    Cambia de nombre el metodo en la intefaz mapper de Articulo

--- src/main/java/com/ramitax/mapper/ArticuloMapper.java
+++ src/main/java/com/ramitax/mapper/ArticuloMapper.java
@@ -1,14 +1,12 @@
 package com.ramitax.mapper;
 
-import com.ramitax.model.Articulo;
+import com.ramitax.model.entity.Articulo;
 import com.ramitax.model.dto.ArticuloDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ArticuloMapper {
 
-    ArticuloMapper INSTANCE = Mappers.getMapper(ArticuloMapper.class);
+    Articulo dtoToArticulo(ArticuloDTO dto);
 
-    Articulo articuloDtoToArticulo(ArticuloDTO dto);
 }

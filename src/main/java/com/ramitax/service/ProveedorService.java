9a2629c846b1d7afbbce766eb40af6b0db910f07
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-11 13:09:46 -0300

    Se agrega la implementacion de Mappeo en Proveedor Service

--- src/main/java/com/ramitax/service/ProveedorService.java
+++ src/main/java/com/ramitax/service/ProveedorService.java
@@ -1,13 +1,31 @@
 package com.ramitax.service;
 
+import com.ramitax.mapper.ProveedorMapper;
+import com.ramitax.model.entity.Proveedor;
+import com.ramitax.model.dto.ProveedorDTO;
 import com.ramitax.repository.ProveedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProveedorService {
+public class ProveedorService implements ProveedorMapper {
 
     @Autowired
     private ProveedorRepository proveedorRepository;
 
+    public Proveedor save(ProveedorDTO dto){
+        Proveedor proveedor = dtoToProveedor(dto);
+        return proveedorRepository.save(proveedor);
+    }
+
+    @Override
+    public Proveedor dtoToProveedor(ProveedorDTO proveedorDTO) {
+        Proveedor proveedor = new Proveedor();
+        proveedor.setNombre(proveedorDTO.getNombre());
+        proveedor.setCuit(proveedorDTO.getCuit());
+        proveedor.setMail(proveedorDTO.getMail());
+        proveedor.setTelefono(proveedorDTO.getTelefono());
+        proveedor.setSaldo(0.0);
+        return proveedor;
+    }
 }

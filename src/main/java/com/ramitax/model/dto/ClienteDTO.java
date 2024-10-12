9135ccbe15da47b23dd8613d9507bea40676acd9
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-12 11:35:12 -0300

    Se agrega campo de tipo lista de Domicilios en Cliente DTO

--- src/main/java/com/ramitax/model/dto/ClienteDTO.java
+++ src/main/java/com/ramitax/model/dto/ClienteDTO.java
@@ -3,9 +3,12 @@ package com.ramitax.model.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class ClienteDTO extends EntidadDTO {
 
     private String dni;
+    private List<DomicilioDTO> domicilios;
 }

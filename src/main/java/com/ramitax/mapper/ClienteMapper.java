3aa8f56f88cb87f3c3aad1e9613d3adb10c634d5
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-15 16:58:11 -0300

    sCrea instancia en la interfaz Mapper de Clientes

--- src/main/java/com/ramitax/mapper/ClienteMapper.java
+++ src/main/java/com/ramitax/mapper/ClienteMapper.java
@@ -3,9 +3,12 @@ package com.ramitax.mapper;
 import com.ramitax.model.entity.Cliente;
 import com.ramitax.model.dto.ClienteDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring", uses = {DomicilioMapper.class})
 public interface ClienteMapper {
+
+    ClienteMapper INSTANCE = Mappers.getMapper(ClienteMapper.class);
 
     Cliente dtoToCliente(ClienteDTO dto);
 

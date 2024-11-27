96b3a3de7de483c06b3326efda94f2c083e2bdab
Ramiro Mendoza <ramamendoza@gmail.com>
2024-11-27 16:39:14 -0300

    Creacion ListaPrecios

---
+++ src/main/java/com/ramitax/model/entity/ListaPrecios.java
@@ -0,0 +1,25 @@
+package com.ramitax.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "lista_precios")
+public class ListaPrecios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Articulo articulo;
+
+    @Column(nullable = false)
+    private Double precio;
+
+}

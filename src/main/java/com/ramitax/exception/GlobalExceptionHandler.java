df1dfed0e24dae9285ac2cfae2c908e5f3cafe13
Ramiro Mendoza <ramamendoza@gmail.com>
2024-11-29 10:42:30 -0300

    Se agregan metodos CRUD a la clase GlobalExceptionHandler

--- src/main/java/com/ramitax/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ramitax/exception/GlobalExceptionHandler.java
@@ -1,8 +1,28 @@
 package com.ramitax.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {CustomException.class})
+    public ResponseEntity<Object> handleEntityException(CustomException ex, WebRequest request){
+        String mensaje = ex.getMessage();
+        return handleExceptionInternal(ex, mensaje, new HttpHeaders(), ex.getHttpStatus(), request);
+    }
+
+    @ExceptionHandler(value = {SQLException.class})
+    public ResponseEntity<Object> handleSQLException(SQLException ex, WebRequest request){
+        String mensaje = ex.getMessage();
+        return handleExceptionInternal(ex, mensaje, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
 }

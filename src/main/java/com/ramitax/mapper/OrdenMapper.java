c075cda6e12a63a8780c37b41f03b2129e55bd00
Ramiro Mendoza <ramamendoza@gmail.com>
2024-12-03 15:10:01 -0300

    Cambia el objeto parametro de metodo en OrdenMapper

--- src/main/java/com/ramitax/mapper/OrdenMapper.java
+++ src/main/java/com/ramitax/mapper/OrdenMapper.java
@@ -1,12 +1,12 @@
 package com.ramitax.mapper;
 
-import com.ramitax.model.dto.OrdenDTO;
+import com.ramitax.model.dto.OrdenCompraDTO;
 import com.ramitax.model.entity.OrdenCompra;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", uses = {OrdenDetalleMapper.class})
 public interface OrdenMapper {
 
-    OrdenCompra dtoToOrden(OrdenDTO dto);
+    OrdenCompra dtoToOrden(OrdenCompraDTO dto);
 
 }

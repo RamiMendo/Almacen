6164f98a3207fc4821c4d3aae1109b05b1271524
Ramiro Mendoza <ramamendoza@gmail.com>
2024-12-03 11:39:16 -0300

    Agrega notacion de EnableAspect en Application

--- src/main/java/com/ramitax/Almacen/AlmacenApplication.java
+++ src/main/java/com/ramitax/Almacen/AlmacenApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.ramitax"})
 @EntityScan(basePackages = {"com.ramitax"})
 @ComponentScan(basePackages = {"com.ramitax"})
+@EnableAspectJAutoProxy
 public class AlmacenApplication {
 
 	public static void main(String[] args) {

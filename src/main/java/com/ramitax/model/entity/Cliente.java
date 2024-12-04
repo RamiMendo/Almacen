bfa0be30b299879959f0fc92f0557ce96aaf09ab
Ramiro Mendoza <ramamendoza@gmail.com>
2024-12-04 16:38:25 -0300

    Se agrega notacion @JoinTable a relacion entre Cliente y Domicilio en la clase Cliente

--- src/main/java/com/ramitax/model/entity/Cliente.java
+++ src/main/java/com/ramitax/model/entity/Cliente.java
@@ -15,13 +15,14 @@ public class Cliente extends Entidad{
     @Column(nullable = false, length = 8)
     private String dni;
 
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinTable(name = "clientes_domicilios",
+            joinColumns = @JoinColumn(name = "id_cliente"),
+            inverseJoinColumns = @JoinColumn(name = "id_domicilio")
+    )
     private List<Domicilio> domicilios;
 
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private List<Pedido> pedidos;
-
-    @OneToOne(mappedBy = "cliente")
-    private UsuarioCliente usuario;
 
 }
